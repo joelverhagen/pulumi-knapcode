890713b774022c14b4424a77fe8b4c766b9166c7
Joel Verhagen <joel.verhagen@gmail.com>
2021-02-20 16:58:53 -0800

    Start on build script

---
+++ build.ps1
@@ -0,0 +1,51 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("Release", "Debug", IgnoreCase = $false)]
+    [string]$Configuration = "Release"
+)
+
+$ErrorActionPreference = "Stop"
+
+if ($Configuration -eq "Debug") {
+    $gcflags = @(-gcflags '-N -l')
+}
+
+$artifacts = Join-Path $PSScriptRoot "artifacts"
+if (!(Test-Path $artifacts)) {
+    New-Item $artifacts -Type Directory | Out-Null
+}
+
+Write-Host "Building Go tools..."
+go build -o $artifacts @gcflags `
+    (Join-Path $PSScriptRoot "cmd\pulumi-resource-knapcode") `
+    (Join-Path $PSScriptRoot "cmd\pulumi-sdkgen-knapcode")
+if ($LASTEXITCODE) { throw "go build failed." }
+
+Write-Host ""
+Write-Host "Deleting current SDK..."
+$sdk = (Join-Path $PSScriptRoot "sdk")
+if (Test-Path $sdk) {
+    Remove-Item $sdk -Recurse -Force
+}
+
+Write-Host ""
+Write-Host "Generating SDK..."
+$schema = Join-Path $PSScriptRoot "schema.json"
+$output = "first run"
+while ($output) {
+    $output = & (Join-Path $artifacts "pulumi-sdkgen-knapcode") $schema $sdk 
+}
+
+Write-Host ""
+Write-Host "Setting dotnet version.txt..."
+$schemaJson = Get-Content $schema | ConvertFrom-Json
+$version = $schemaJson.version
+$version | Set-Content (Join-Path $sdk "dotnet\version.txt") -Encoding ASCII
+
+Write-Host ""
+Write-Host "Building NuGet package ..."
+dotnet build (Join-Path $sdk "dotnet\Pulumi.Knapcode.csproj") `
+    "/p:Version=$version" `
+    "/p:PackageOutputDir=$artifacts" `
+    -c $Configuration
